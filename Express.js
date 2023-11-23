75f0b29615ede87429d08a87a8058286ecbd3929
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-11-22 20:13:02 -0500

    part 3

--- Express.js
+++ Express.js
@@ -2,11 +2,42 @@ const express = require('express');
 const app = express();
 const port = 3001; // or any other port you prefer
 const cors = require('cors');
+const mysql = require('mysql2');
 
 
 app.use(cors());
 
-app.get('/', (req, res) => {
+
+// const db  = mysql.createPool({
+//   connectionLimit : 10,
+//   host            : 'localhost',
+//   user            : 'root',
+//   password        : 'Rociotoro1!',
+//   database        : 'roboticsinformation'
+// });
+
+const db  = mysql.createConnection({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : 'Rociotoro1!',
+  database        : 'roboticsinformation'
+});
+
+
+app.post('/signup', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+     
+    //request to insert something into the database 
+    db.query("INSERT INTO users (username, password) VALUES (?,?)",[username, password],(err, result) => {
+      if(err){
+        console.log(err)
+      }else{
+        res.send({username: username})
+      }
+    } )
+    
   // Handle your API logic here
   res.send('Like and Suscribe: ')
  
